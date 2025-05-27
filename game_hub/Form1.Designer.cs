c30f39b0b4545b40ecbf230923c0b2676884035a
mlich <mlichmatus0@gmail.com>
2025-05-27 20:42:24 +0200

    Add project files.

---
+++ game_hub/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace game_hub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_input = new System.Windows.Forms.TextBox();
+            this.password_input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.register_button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(93, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(93, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "password:";
+            // 
+            // username_input
+            // 
+            this.username_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.username_input.Location = new System.Drawing.Point(183, 179);
+            this.username_input.Name = "username_input";
+            this.username_input.Size = new System.Drawing.Size(122, 26);
+            this.username_input.TabIndex = 3;
+            // 
+            // password_input
+            // 
+            this.password_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.password_input.Location = new System.Drawing.Point(183, 218);
+            this.password_input.Name = "password_input";
+            this.password_input.PasswordChar = '*';
+            this.password_input.Size = new System.Drawing.Size(122, 26);
+            this.password_input.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Linen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(102, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // register_button
+            // 
+            this.register_button.AutoSize = true;
+            this.register_button.BackColor = System.Drawing.Color.Linen;
+            this.register_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.register_button.Location = new System.Drawing.Point(210, 293);
+            this.register_button.Name = "register_button";
+            this.register_button.Size = new System.Drawing.Size(95, 30);
+            this.register_button.TabIndex = 6;
+            this.register_button.Text = "Register";
+            this.register_button.UseVisualStyleBackColor = false;
+            this.register_button.Click += new System.EventHandler(this.register_button_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(93, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Log in / Register";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(418, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.register_button);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.password_input);
+            this.Controls.Add(this.username_input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Log in";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_input;
+        private System.Windows.Forms.TextBox password_input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button register_button;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ game_hub/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace game_hub
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = username_input.Text;
+            string password = password_input.Text;
+
+            if (username == "admin" && password == "admin")
+            {
+                MessageBox.Show("loged in as " + username);
+                new MainMenu().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("nieje v databaze");
+            }
+
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void register_button_Click(object sender, EventArgs e)
+        {
+            string username = username_input.Text;
+            string password = password_input.Text;
+
+            if (username != string.Empty && password != string.Empty)
+            {
+                MessageBox.Show("Registered in as "+ username);
+                new MainMenu().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("nieje v databaze");
+            }
+        }
+
+       
+    }
+}
